1bc0bcc6ea28eb4feda7f5f942905f1ea627ec7f
THE LUJAIN <silversocer17@gmail.com>
2023-12-24 16:58:36 +0300

    new

---
+++ phase3/App.js
@@ -0,0 +1,12 @@
+function App(){
+    return( <div>
+        <card>
+        <ol>
+            <li>Testing 1</li>
+            <li>Tesitng 2</li>
+            <li>Testing 3</li>
+            </ol>    
+        </card>       
+    </div>
+    );
+}
